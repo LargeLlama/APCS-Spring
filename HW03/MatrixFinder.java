714e6fc56c7e5214a36813bf85f0920702423b66
Rubin <rubinpeci@yahoo.com>
2018-02-01 09:29:10 -0500

    Add HW03

---
+++ HW03/MatrixFinder.java
@@ -0,0 +1,20 @@
+public class MatrixFinder {
+	
+	public static String linSearch(int[][] matrix, int desiredValue) {
+		int currentRow = 0;
+		int currentColumn = matrix.length - 1;
+
+		String retString = "(-1, -1)";
+
+		while (currentColumn >= 0 && currentRow < matrix.length) {
+			if (matrix[currentRow][currentColumn] == desiredValue)
+				retString = "(" + currentRow + ", " + currentColumn + ")";
+			else if (matrix[currentRow][currentColumn] < desiredValue)
+				currentRow++;
+			else
+				currentColumn--;
+		}
+
+		return retString;
+	}
+}

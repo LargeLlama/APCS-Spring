2d55f2680432016bf965bec097f89c4f3975eaa8
Rubin Peci <rpeci@stuy.edu>
2018-02-27 22:43:30 -0500

    Complete HW10

--- HW10/QueenBoard.java
+++ HW10/QueenBoard.java
@@ -30,7 +30,7 @@ public class QueenBoard
    */
   public boolean solve()
   {
-    return false;
+	  return solveH(0); //gotta start from the basics
   }
 
   
@@ -39,17 +39,41 @@ public class QueenBoard
    */
   private boolean solveH( int col ) 
   {
-    return false;
+	  if (col >= _board.length) {		//once we reach the end of the line, we're done
+		  return true;
+	  }else {							//otherwise, we keep fighting
+		  for (int i = 0; i < _board.length; i++) {	//loop thru each row
+			  if (addQueen(i, col)) { 				//if u can add a queen
+				  addQueen(i, col);					//then add it 
+	
+				  if (solveH(col + 1))		//normally i'd return this value but there is an else portion to it
+					  return true;
+				  removeQueen(i, col);		//if this configuration does not work we have to remove the original queen and try again
+			  }
+		  }
+	  }
+
+	  return false;		//if we try everything and it no work then its a falso
   }
 
-
-  public void printSolution()
-  {
     /** Print board, a la toString...
         Except:
         all negs and 0's replaced with underscore
         all 1's replaced with 'Q'
     */
+
+  public void printSolution()
+  {
+	  for (int i = 0; i < _board.length; i++) {
+		  for (int n = 0; n < _board[i].length; n++) {
+			  if (_board[i][n] <= 0)
+				System.out.print("_\t");
+			  else
+				System.out.print("\u001B[31mQ\u001B[0m\t");
+
+		  }
+		  System.out.println("\n");
+	  }
   }
 
 
@@ -63,6 +87,7 @@ public class QueenBoard
    */
   private boolean addQueen(int row, int col){
     if(_board[row][col] != 0){	//a value not equal to zero indicates a queen is present, thus the spot is occupied and cannot be placed
+								//a value not equal to zero also indicates that a queen cannot be placed there
 	    return false;	
     }
 	//otherwise, we set the value at the given coordinate to 1
@@ -95,14 +120,14 @@ public class QueenBoard
    * postcondition: return a boolean that indicates that a queen can be removed
    */
   private boolean removeQueen(int row, int col){
-    if ( _board[row][col] != 1 ) {
-	    return false;
+    if ( _board[row][col] != 1 ) {	//confirms there is a queen to be removed
+	    return false;	
     }
     _board[row][col] = 0;
-    int offset = 1;
+    int offset = 1;	//sets the initial offset of 1
 
-    while( col+offset < _board[row].length ) {
-	    _board[row][col+offset]++;
+    while( col+offset < _board[row].length ) {		//same as the addQueen method, except it incremements instead of decrements
+	    _board[row][col+offset]++;	
 
 	    if( row - offset >= 0 ) {
 	        _board[row-offset][col+offset]++;
@@ -140,12 +165,28 @@ public class QueenBoard
   public static void main( String[] args )
   {
     QueenBoard b = new QueenBoard(5);
+	/*=====================yeet bar=================
     System.out.println(b);
     b.addQueen(3,0);
     b.addQueen(0,1);
     System.out.println(b);
     b.removeQueen(3,0);
     System.out.println(b);
+	===============================================*/
+	System.out.println("Board size: 5x5");
+	b.solve();
+	b.printSolution();
+
+	//this doesn't have anysolutions, wanted to make sure it wouldn't break the code!
+	System.out.println("Board size: 2x2");
+	QueenBoard c = new QueenBoard(2);
+	c.solve();
+	c.printSolution();
+
+	System.out.println("Board size: 8x8");
+	QueenBoard d = new QueenBoard(8);
+	d.solve();
+	d.printSolution();
   }
     
 }//end class

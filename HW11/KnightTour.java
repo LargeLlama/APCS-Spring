561adfd3c4836b8c3c01084dbeddb06650033153
Rubin Peci <rpeci@stuy.edu>
2018-02-28 09:30:49 -0500

    Add hw11

---
+++ HW11/KnightTour.java
@@ -0,0 +1,17 @@
+//Wubin Peco
+//APCS pd02
+//HW11 - Knight's Tour - tickets are only $39.99! Watch now!
+//2018-02-28
+
+public class KnightTour {
+
+	public int[][] _board;
+
+	public KnightTour() {
+		_board = new int[8][8];
+	}
+
+	public boolean solve() {
+		
+	}
+}

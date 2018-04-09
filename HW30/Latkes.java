a583816c85e20ba5b18b2d8268145f984baf4d7a
Rubin Peci <rubinpeci@gmail.com>
2018-04-09 23:52:17 +0000

    Update HW30

--- HW30/Latkes.java
+++ HW30/Latkes.java
@@ -7,66 +7,69 @@
  * skeleton for class Latkes
  * Implements a stack of Strings using an encapsulated array
  *****************************************************/
-
 public class Latkes
 {
-  private String [] _stack;
-  private int _stackSize;
+  private String [] _stack; //immutable
+  private int _stackSize;	//keep track of # of elements
 
   //constructor
   public Latkes( int size )
-  {
-	_stackSize = size;
-	_stack = new String[size];
+  {	
+	_stackSize = 0;	//will always be 0 at first
+	_stack = new String[size];	//will be limited to the given size
   }
 
   //means of insertion
   public void push( String s )
   {
-
-	for(int i = 0; i < _stackSize; i++) {
-		if (_stack[i] == null) {
-			_stack[i] = s;
-			break;
-		}
+	//we only push if it isn't full
+	//normally, would through a StackOverflowError but in this case
+	//we ignore illegal additions
+	if (!isFull()) {
+		_stack[_stackSize] = s;
+		_stackSize++;	
+		return;
 	}
-		
   }
 
   //means of removal
   public String pop()
   {
-	
+	//can only pop if there is an item to pop
+	if(!isEmpty()) {
+		String tmp = _stack[_stackSize - 1];	//store tmp string
+		_stack[_stackSize - 1] = null;			//set back to null
+		_stackSize--;							//decrement stack size
+		return tmp;								//return the temp string 
+	}
+	return null;		//otherwise nothing was removed so we return null
   }
 
   //chk for emptiness
   public boolean isEmpty()
   {
-	return true;
+	return _stackSize == 0;	//is empty when stack size is 0 - ez
   }
 
   //chk for fullness
   public boolean isFull()
   {
-	return true;
+	return _stackSize == _stack.length;	//is full when the # of elements is = to the length
+  
   }
-
-//==============HELPER================
-  public String printArray() {
-	String retString = "";
+//=========HELPER METHOD=========//
+//prints out the stack to keep track of the items in there
+  public void printStack() {
 	for (String s : _stack) 
-		retString += (s + ", ");
-	return retString;
-  }
-  public String toString() {
-	return printArray();
+		System.out.print(s + ", ");
+	System.out.println();
   }
 
   //main method for testing
   public static void main( String[] args )
   {
-      Latkes tastyStack = new Latkes(10);
-	  System.out.println(tastyStack);
+      Latkes tastyStack = new Latkes(10); //init a new latkes obj of size 10
+	  //push all these strings to the stack
       tastyStack.push("aoo");
       tastyStack.push("boo");
       tastyStack.push("coo");
@@ -77,11 +80,14 @@ public class Latkes
       tastyStack.push("hoo");
       tastyStack.push("ioo");
       tastyStack.push("joo");
+//==========extra============
+//these will NOT be pushed because the stack is full at this point
       tastyStack.push("coocoo");
       tastyStack.push("cachoo");
 	
+	  //print the memory address of the tastyStack obj and the contents of the stack
 	  System.out.println(tastyStack);
-    /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+	  tastyStack.printStack();
 
       //cachoo
       System.out.println( tastyStack.pop() );
@@ -107,9 +113,12 @@ public class Latkes
       System.out.println( tastyStack.pop() );
       //aoo
       System.out.println( tastyStack.pop() );
+	  //idk what this top is going on about the stack cant be filled past its limit lol
 
       //stack empty by now; SOP(null)
       System.out.println( tastyStack.pop() );
+      /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+
       ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
 
   }//end main()

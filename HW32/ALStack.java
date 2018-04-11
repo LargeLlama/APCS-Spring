04ddf972a1d896cd6307b230371154ecd4ccc128
Rubin Peci <rubinpeci@gmail.com>
2018-04-11 09:25:33 +0000

    Update HW32

---
+++ HW32/ALStack.java
@@ -0,0 +1,33 @@
+//Wubin Peco
+//APCS-2 pd02
+//hw32 yurd
+//2018-04-11
+import java.util.ArrayList;
+
+public class ALStack<T> implements Stack<T> {
+	private ArrayList<T> _stack;
+	private int _stackSize;
+
+	public ALStack(int size) {
+		_stack = new ArrayList<T>(size);
+		_stackSize = 0;
+	}
+	
+	public boolean isEmpty() {
+		return _stackSize == 0;
+	}
+
+	public T peek() {
+		return _stack.get(_stackSize - 1);
+	}
+
+	public T pop() {
+		T temp = _stack.remove(_stackSize - 1);
+		return temp;
+	}
+
+	public void push (T x) {
+		_stack.set(_stackSize, x);
+		_stackSize++;
+	}
+}

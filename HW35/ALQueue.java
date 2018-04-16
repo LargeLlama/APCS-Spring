4e3770d4efb04a6c0d31a9387fb19a19bacc4f0e
Rubin Peci <rubinpeci@gmail.com>
2018-04-16 09:25:11 +0000

    Update HW34

---
+++ HW35/ALQueue.java
@@ -0,0 +1,29 @@
+//KEKE - Ganiel Delfand and Wubin Peco
+//APCS-2 pd02
+//HW35
+//2018-04-16
+import java.util.ArrayList;
+public class ALQueue<Quasar> implements Queue<Quasar> {
+	ArrayList<Quasar> _list;
+
+	public ALQueue() {
+		_list = new ArrayList<Quasar>();
+	}
+	
+	public Quasar dequeue() {
+		Quasar temp = _list.remove(0);
+		return temp;
+	} 
+	
+	public void enqueue(Quasar element) {
+		_list.add(element);
+	}
+	
+	public boolean isEmpty() {
+		return _list.size() == 0;
+	}
+
+	public Quasar peekFront() {
+		return _list.get(0);
+	}
+}
